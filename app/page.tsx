30c530837b2ebf64a82f359cee3b92790b96e4b1
takatama <takatama@users.noreply.github.com>
2023-12-31 22:15:49 +0900

    revised contents

--- app/page.tsx
+++ app/page.tsx
@@ -16,9 +16,9 @@ export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
   return (
     <div className={darkMode ? "dark" : ""}>
-      <main className="bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900">
+      <main className="bg-white px-5 md:px-20 lg:px-40 dark:bg-gray-900">
         <section className="min-h-screen">
-          <nav className="py-10 mb-12 flex justify-between">
+          <nav className="py-5 mb-6 flex justify-between">
             <h1 className="text-xl"></h1>
             <ul className="flex items-center">
               <li>
@@ -38,20 +38,21 @@ export default function Home() {
               </li>
             </ul>
           </nav>
-          <div className="text-center p-10">
+          <div className="text-center p-5">
             <h2 className="text-5xl py-2 text-blue-800 font-medium md:text-6xl dark:text-cyan-500">
               Hirokazu TAKATAMA
             </h2>
             <h3 className="text-2xl py-2 md:text-3xl dark:text-white">
-              IT and Technical Talent Development Trainer.
+              Technical Trainer and Process Improvement Engineer.
             </h3>
             <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto dark:text-white">
-              Skilled in programming, AI implementation, and spearheading
-              innovative technology projects for organizational advancement.
-              Based in Tokyo, Japan.
+              Skilled engineer specializing in technical training, leveraging
+              programming expertise to educate and innovate. Proficient in
+              enhancing business processes through tech-driven solutions and
+              software engineering skills. Based in Tokyo, Japan.
             </p>
           </div>
-          <div className="text-5xl flex justify-center gap-16 text-gray-600">
+          <div className="text-5xl flex justify-center gap-16 text-gray-600 dark:text-gray-400">
             <a href="https://twitter.com/takatama_jp" target="_blank">
               <AiFillTwitterCircle />
             </a>
